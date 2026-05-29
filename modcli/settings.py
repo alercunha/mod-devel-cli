a6ced677bf1d4f2f4f33ea2d087d80687b5ef782
Alex Cunha <alex@mod.audio>
2026-05-29 09:26:44 -0700

    Update API and pipeline URLs to mod.audio

--- modcli/settings.py
+++ modcli/settings.py
@@ -2,7 +2,7 @@ import os
 
 CONFIG_DIR = os.path.expanduser('~/.config/modcli')
 URLS = {
-    'labs': ('https://api-labs.moddevices.com/v2', 'https://pipeline-labs.moddevices.com/bundle/'),
-    'dev': ('https://api-dev.moddevices.com/v2', 'https://pipeline-dev.moddevices.com/bundle/'),
+    'labs': ('https://api-labs.mod.audio/v2', 'https://pipeline-labs.mod.audio/bundle/'),
+    'dev': ('https://api-dev.mod.audio/v2', 'https://pipeline-dev.mod.audio/bundle/'),
 }
 DEFAULT_ENV = 'labs'
